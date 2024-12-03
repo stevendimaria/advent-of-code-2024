542bfd5138e3861edc15bda56de524a5e30ebec4
stevendimaria <dimariasteve@gmail.com>
2024-12-03 09:40:24 -0500

    Day 3.2

--- day3/day3.py
+++ day3/day3.py
@@ -1,25 +1,53 @@
 import re
 
+PATTERN = r"mul\([0-9]+,[0-9]+\)"
 CHARS = ''
 with open('input.txt') as file:
     for line in file:
         CHARS += line
 
 
-def part1():
-    calcs = re.findall(r"mul\([0-9]+,[0-9]+\)", CHARS)
+def part1(find_chars: bool = True, calc: str = None):
+    if find_chars:
+        calcs = re.findall(PATTERN, CHARS)
+    else:
+        calcs = [calc]
 
     ans = 0
     for calc in calcs:
         first, second = calc.split(',')
         first = int(first.split('(')[1])
         second = int(second[:-1])
-        ans += first*second
+        ans += first * second
 
     return ans
 
 
-if __name__=='__main__':
+def part2():
+    triggers = CHARS.split('do')
+    calcs = []
+    for trigger in triggers:
+        if trigger.startswith('()'):
+            calcs.append('ON')
+        if trigger.startswith("n't()"):
+            calcs.append('OFF')
 
+        calcs += re.findall(PATTERN, trigger)
+
+    ans, flag = 0, True
+    for calc in calcs:
+        if calc == 'ON':
+            flag = True
+        elif calc == 'OFF':
+            flag = False
+        elif flag:
+            ans += part1(False, calc)
+        else:
+            continue
+
+    return ans
+
+
+if __name__ == '__main__':
     print(f'Part 1 : {part1()}')
-    # print(f'Part 2 : {part_2_ans}')
+    print(f'Part 2 : {part2()}')
