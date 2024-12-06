d1a8142d694a614f673fadf1bcdd1400734fea88
stevendimaria <dimariasteve@gmail.com>
2024-12-06 08:24:27 -0500

    Day 5.2

--- day5/day5.py
+++ day5/day5.py
@@ -1,3 +1,6 @@
+from collections import deque
+from tqdm import tqdm
+
 class Node:
 
     def __init__(self, page: str):
@@ -6,44 +9,62 @@ class Node:
 
 
 with open("input.txt") as file:
-    RULES = []
+    RULES = {'': set()}
     UPDATES = []
+    STARTS = set()
     for line in file:
         line = line[:-1]
         if '|' in line:
-            RULES.append(line.split('|'))
+            k, v = line.split('|')
+            RULES[''] |= {k}
+            if not RULES.get(k):
+                RULES[k] = set()
+            RULES[k] |= {v}
+            STARTS |= {k}
         elif ',' in line:
             UPDATES.append(line.split(','))
         else:
             continue
 
-
 def part1():
-    nodes = {'start': Node('start')}
-    for first, second in RULES:
-        if not nodes.get(first):
-            nodes[first] = Node(first)
-            nodes['start'].children.append(nodes[first])
-
-        if not nodes.get(second):
-            nodes[second] = Node(second)
-            nodes['start'].children.append(nodes[second])
-
-        nodes[first].children.append(nodes[second])
-
-    ans = 0
+    ans, _incorrect = 0, []
     for _update in UPDATES:
-        curr = nodes['start']
-        for n in _update:
-            if nodes.get(n) in curr.children:
-                curr = nodes[n]
-            else:
+        curr = _update[0]
+        for n in _update[1:]:
+            if n not in STARTS or n not in RULES[curr]:
+                _incorrect.append(_update)
                 break
+            curr = n
         else:
             ans += int(_update[(len(_update)//2)])
+
+    return ans, _incorrect
+
+def part2(incorrect: list):
+    ans = 0
+    # good = {}
+
+    for update in tqdm(incorrect):
+        q = deque([('', [x for x in update], [])])
+        while q:
+            curr, rem, out = q.popleft()
+
+            if not rem:
+                if curr in RULES[out[-1]]:
+                    out = out[1:]+[curr]
+                    ans += int(out[len(out)//2])
+                break
+
+            for i, n in enumerate(rem):
+                if n not in STARTS and len(rem)>1:
+                    continue
+                if n in RULES[curr]:
+                    q.appendleft((n, rem[:i]+rem[i+1:], out+[curr]))
     return ans
 
 
 if __name__=='__main__':
-    print(f"Part 1 : {part1()}")
-    # print(f"Part 2 : {part2()}")
+    part1_ans, wrong = part1()
+
+    print(f"Part 1 : {part1_ans}")
+    print(f"Part 2 : {part2(wrong)}")
