c884594f761e46241c6e8eb65379d1b05c091b2b
stevendimaria <dimariasteve@gmail.com>
2024-12-06 08:37:56 -0500

    Formatted

--- day5/day5.py
+++ day5/day5.py
@@ -1,30 +1,31 @@
 from collections import deque
 from tqdm import tqdm
 
-class Node:
 
+class Node:
     def __init__(self, page: str):
         self.page = page
         self.children = []
 
 
 with open("input.txt") as file:
-    RULES = {'': set()}
+    RULES = {"": set()}
     UPDATES = []
     STARTS = set()
     for line in file:
         line = line[:-1]
-        if '|' in line:
-            k, v = line.split('|')
-            RULES[''] |= {k}
+        if "|" in line:
+            k, v = line.split("|")
+            RULES[""] |= {k}
             if not RULES.get(k):
                 RULES[k] = set()
             RULES[k] |= {v}
             STARTS |= {k}
-        elif ',' in line:
-            UPDATES.append(line.split(','))
+        elif "," in line:
+            UPDATES.append(line.split(","))
         else:
             continue
+
 
 def part1():
     ans, _incorrect = 0, []
@@ -36,34 +37,34 @@ def part1():
                 break
             curr = n
         else:
-            ans += int(_update[(len(_update)//2)])
+            ans += int(_update[(len(_update) // 2)])
 
     return ans, _incorrect
 
+
 def part2(incorrect: list):
     ans = 0
-    # good = {}
 
     for update in tqdm(incorrect):
-        q = deque([('', [x for x in update], [])])
+        q = deque([("", [x for x in update], [])])
         while q:
             curr, rem, out = q.popleft()
 
             if not rem:
                 if curr in RULES[out[-1]]:
-                    out = out[1:]+[curr]
-                    ans += int(out[len(out)//2])
+                    out = out[1:] + [curr]
+                    ans += int(out[len(out) // 2])
                 break
 
             for i, n in enumerate(rem):
-                if n not in STARTS and len(rem)>1:
+                if n not in STARTS and len(rem) > 1:
                     continue
                 if n in RULES[curr]:
-                    q.appendleft((n, rem[:i]+rem[i+1:], out+[curr]))
+                    q.appendleft((n, rem[:i] + rem[i + 1 :], out + [curr]))
     return ans
 
 
-if __name__=='__main__':
+if __name__ == "__main__":
     part1_ans, wrong = part1()
 
     print(f"Part 1 : {part1_ans}")
