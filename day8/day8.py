0c1780adb2548b82bb9a827e3d771d8f132c67bd
stevendimaria <dimariasteve@gmail.com>
2024-12-09 12:05:26 -0500

    Day 8.2

--- day8/day8.py
+++ day8/day8.py
@@ -6,28 +6,44 @@ ROWS = len(MAP)
 COLS = len(MAP[0])
 for r in range(len(MAP)):
     for c in range(len(MAP[0])):
-        if MAP[r][c]=='.':
+        if MAP[r][c] == ".":
             continue
 
         if not LOCATIONS.get((ch := MAP[r][c])):
             LOCATIONS[ch] = []
-        LOCATIONS[ch].append((r,c))
+        LOCATIONS[ch].append((r, c))
 
 
-def part1():
+def part1(part2=False):
     antinodes = set()
     for _, locs in LOCATIONS.items():
-        # antinodes[k] = {}
         for i in range(len(locs)):
-            # antinodes[k][locs[i]] = []
             for j in range(len(locs)):
-                if i==j:
+                if i == j:
                     continue
-                an = (locs[i][0]+(locs[i][0]-locs[j][0])), (locs[i][1]+(locs[i][1]-locs[j][1]))
-                if 0<=an[0]<ROWS and 0<=an[1]<COLS:
-                    antinodes.add(an)
+
+                diff = (locs[i][0] - locs[j][0], locs[i][1] - locs[j][1])
+                if not part2:
+                    an = (locs[i][0] + diff[0]), (locs[i][1] + diff[1])
+                    if 0 <= an[0] < ROWS and 0 <= an[1] < COLS:
+                        antinodes.add(an)
+                else:
+                    an = ((locs[i][0]), (locs[i][1]))
+                    while 0 <= an[0] < ROWS and 0 <= an[1] < COLS:
+                        antinodes.add(an)
+                        an = (an[0] + diff[0], an[1] + diff[1])
+
+                    an = (locs[i][0]), (locs[i][1])
+                    while 0 <= an[0] < ROWS and 0 <= an[1] < COLS:
+                        antinodes.add(an)
+                        an = (an[0] - diff[0], an[1] - diff[1])
     return len(antinodes)
 
 
-if __name__=='__main__':
+def part2():
+    return part1(part2=True)
+
+
+if __name__ == "__main__":
     print(f"Part 1 : {part1()}")
+    print(f"Part 2 : {part2()}")
