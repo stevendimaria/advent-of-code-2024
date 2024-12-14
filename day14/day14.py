3ec467b8de29477bb86ac02b87cb17b1d6056e67
stevendimaria <dimariasteve@gmail.com>
2024-12-14 11:11:19 -0500

    Day 14.2 unga bunga

--- day14/day14.py
+++ day14/day14.py
@@ -1,4 +1,5 @@
 from math import prod
+from tqdm import tqdm
 
 
 with open("input.txt", "r") as file:
@@ -26,44 +27,69 @@ for i, data in enumerate(INPUT):
     ROBOTS[i+1] = {'pos':(pos_x, pos_y), 'vel':(vel_x, vel_y)}
 
 
-def part1(robots):
-    for sec in range(100):
+def part1(robots, _map, seconds, part=1):
+    def calc_safety_factor():
+        quadrants = [0, 0, 0, 0]
+        for k, v in robots.items():
+            c, r = v['pos']
+
+            if c < 50 and r < 51:
+                quadrants[0] += 1
+            elif c > 50 and r < 51:
+                quadrants[1] += 1
+            elif c < 50 and r > 51:
+                quadrants[2] += 1
+            elif c > 50 and r > 51:
+                quadrants[3] += 1
+            else:
+                continue
+
+        return prod(quadrants)
+
+    all_safety_factors = []
+    for sec in tqdm(range(seconds)):
         for k, v in robots.items():
             pos, vel = v['pos'], v['vel']
 
             x, y = pos
+            #########
+            if part==2:
+                _map[y][x] -= 1
+            #########
             x += vel[0]
             y += vel[1]
 
             x = x%101 if 0<=x else x+101
             y = y%103 if 0<=y else y+103
 
+            #########
+            if part==2:
+                _map[y][x] += 1
+                sf = calc_safety_factor()
+                all_safety_factors.append((sec, sf))
+                if sf<100000000:
+                    print(sec)
+                    f = open('part2.txt', 'a')
+                    f.write(f"{sec}\n")
+                    for line in _map:
+                        f.write(f"{line}\n")
+                    f.close()
+            #########
+
             robots[k]['pos'] = (x, y)
 
-    quadrants = [0,0,0,0]
-    for k,v in robots.items():
-        c, r = v['pos']
+    print(sorted(all_safety_factors)[:20])
+    return calc_safety_factor()
 
-        if c<50 and r<51:
-            quadrants[0] += 1
-        elif c>50 and r<51:
-            quadrants[1] += 1
-        elif c<50 and r>51:
-            quadrants[2] += 1
-        elif c>50 and r>51:
-            quadrants[3] += 1
-        else:
-            continue
 
-    return prod(quadrants)
 
-def part2():
-    return 0
+def part2(robots, _map, seconds):
+    return part1(robots, _map, seconds, part=2)
 
 
 if __name__ == "__main__":
-    print(f"Part 1 : {part1(ROBOTS)}")
-    print(f"Part 2 : {part2()}")
+    print(f"Part 1 : {part1(ROBOTS, MAP, 100)}")
+    print(f"Part 2 : {part2(ROBOTS, MAP, 10000)}")
 
 
 
